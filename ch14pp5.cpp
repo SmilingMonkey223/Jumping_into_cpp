bb31e72d40235689150060078f64bbd9dee709f4
SmilingMonkey223 <mistralow@gmail.com>
2024-08-19 14:44:09 +0200

    Laptop 19.08

--- ch14pp5.cpp
+++ ch14pp5.cpp
@@ -3,30 +3,34 @@
 
 using namespace std;
 
-void BoardInit(vector<vector<char>> &board, int height, int width);
-bool checkValidMove();
-void updateBoard();
+void createBoard(vector<vector<char>> &board, int height, int width);
+bool checkValidMove(vector<vector<char>> &board, int height, int userInput);
+void updateBoard(vector<vector<char>> &board, int height, int userInput,
+                 int width);
 
 int main() {
   vector<vector<char>> board;
-  int height, width;
+  int height, width, userInput, turn;
   cout << " ^-^ Welcome to my Version of the game Connect-4 ^-^ \n";
   cout << "P1 is + and P2 is x, Please input the height and width of the game "
           "you guys want to create, it has to be a minimum of 4x4 obviously \n";
   while (height < 4 && width < 4) {
     cin >> height >> width;
   }
-  BoardInit(vector<vector<char>> board, height, width);
+  turn = height * width;
+  createBoard(vector<vector<char>> board, height, width);
   cout << "P1 please make ur move, write the number where you want to place ur "
           "symbol \n";
-  if (checkValidMove = true) {
+  cin >> userInput;
+  while (checkValidMove(userInput) = false {
+    cout << "That was an invalid move, your input has to be a number between 1 "
+            "and the width of the game \n";
+    cin >> userInput;
     updateBoard();
-  } else {
-    cout << "That was an invalid move, cant play like this \n";
-  }
+    }
 }
 
-void BoardInit(vector<vector<char>> &board, int height, int width) {
+void createBoard(vector<vector<char>> &board, int height, int width) {
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++) {
       board[i][j] = '_';
@@ -34,3 +38,15 @@ void BoardInit(vector<vector<char>> &board, int height, int width) {
   }
   cout << vector<vector<char>> & board;
 }
+
+bool checkValidMove(vector<vector<char>> &board, int height, int userInput) {
+  for (int i = height; i >= 0; i--) {
+    if (board[height][userInput] == '_') {
+      return true;
+    }
+  }
+  return false;
+}
+
+void updateBoard(vector<vector<char>> &board, int height, int userInput,
+                 int width) {}
