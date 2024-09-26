d22f63e73de63fd7a038a419afdea93e03da1adc
SmilingMonkey223 <mistralow@gmail.com>
2024-09-26 13:14:49 +0200

    ch14pp5pointer, new structure

--- ch14pp5pointer.cpp
+++ ch14pp5pointer.cpp
@@ -1,65 +1,76 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-void createBoard(vector<vector<int>> &board, int height, int width);
-int checkValidMove(vector<vector<int>> &board, int playerMove, int height);
-void updateBoard(vector<vector<int>> &board, int playerMove, int height,
-                 int width);
-void checkWin(vector<vector<int>> &board);
+void createBoard(char **board, int height, int width);
+void updateBoard(char **board, int height, int width);
+bool PlayerMoveValid(char **board, int playerMove, int height, int width);
+void checkWin(char **board);
 
 int main() {
-  int height, width, playerMove;
-  vector<vector<int>> board;
+  int height, width, draw, playerMove, turn = 0;
   cout << "Welcome to my version of a Connect-4 *-*\n";
   cout << "Player 1 will be the symbol '+' and Player 2 'x' \n";
   cout << "You will first choose what size the board is, please first input a "
           "height and after that a width \n";
   cout << "The board can be a minimum of 4x4 \n";
   cin >> height >> width;
+  draw = width * height;
   while (height < 5 || width < 5) {
     cout << "invalid board size, got to be atleast 4x4.... \n";
     cout << "Please input another height and width \n";
     cin >> height >> width;
   }
-  createBoard(board, height, width);
-  while (true) {
-    int turn = 0;
-    if (turn % 2 == 0) {
-      cout << "Alright, P1 please write the number for which spot you would "
-              "like "
-              "to occupy";
-    } else {
-      cout << "Alright, P2 please write the number for which spot you would "
-              "like "
-              "to occupy";
-    }
-    turn++;
+  char **board = new char *[height];
+  createBoard(int height, int width);
+  cout << "Alright, write down the width (as a number) where you want to "
+          "make ur move \n";
+  while (turn <= draw) {
+    cout << "P1 turn \n";
     cin >> playerMove;
-    checkValidMove(board, playerMove, height);
-    updateBoard(board, playerMove, height, width);
-    checkWin(board);
+    while (!PlayerMoveValid) {
+      cout << "That was an invalid move, ur supposed to write a number between "
+              "1 "
+              "and the width you specified for the board, try again \n";
+      cin >> playerMove;
+    }
+    updateBoard(int height, int width);
+    checkWin();
+    turn++;
+    cout << "P2 turn \n";
+    cin >> playerMove;
+    while (!PlayerMoveValid) {
+      cout << "That was an invalid move, ur supposed to write a number between "
+              "1 "
+              "and the width you specified for the board, try again \n";
+      cin >> playerMove;
+    }
+    updateBoard(int height, int width);
+    checkWin();
+    turn++;
   }
+  cout << "the game has concluded without a winner, hope you enjoyed playing";
+  return 0;
 }
 
-void createBoard(vector<vector<int>> *board, int height, int width) {
+void createBoard(char **board, int height, int width) {
   for (int i = 0; i < height; i++) {
+    board[i] = new char[width];
     for (int j = 0; j < width; j++) {
-      *board[i][j] = "_" << "|" << endl;
+      board[i][j] = '_';
+      cout << board[i][j] << " ";
     }
     cout << endl;
   }
 }
 
-int checkValidMove(vector<vector<int>> &board, int playerMove, int height) {
-  for (int i = height; i >= 0; i--) {
-    if (board[playerMove] == "_") {
-      return board[i][playerMove];
+void updateBoard(char **board, int height, int width) {}
+
+bool PlayerMoveValid(char **board, int playerMove, int height, int width) {
+  for (int i = height; i > 0; i--) {
+    if (board[i] = "_") {
+      return true;
     }
   }
+  return false;
 }
-void updateBoard(vector<vector<int>> &board, int playerMove, int height,
-                 int width) {}
-
-void checkWin(vector<vector<int>> &board) {}
